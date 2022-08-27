676193990dcb8b4b031b879cf022ba6e107d7941
DeSpecTDr <73001251+DeSpecTDr@users.noreply.github.com>
2022-08-27 03:21:06 +0300

    Update

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -7,7 +7,6 @@
   home.packages = with pkgs; [
     firefox-wayland # TODO: switch to librewolf
     # texlive.combined.scheme-full # latex
-    element-desktop-wayland # -wayland???
     thunderbird-wayland # -wayland???
   ];
   
